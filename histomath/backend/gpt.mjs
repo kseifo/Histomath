9931fda6babdaaf7721df0a816a8ace56e009a17
g64462ks <kareem.seifo@student.manchester.ac.uk>
2024-11-09 18:05:29 +0000

    Setup of opencv api

---
+++ histomath/backend/gpt.mjs
@@ -0,0 +1,19 @@
+import OpenAI from "openai";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+console.log(process.env.OPEN_API_KEY);
+const openai = new OpenAI({apiKey:process.env.OPEN_API_KEY});
+
+const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        {
+            role: "user",
+            content: "Write a haiku about recursion in programming.",
+        },
+    ],
+});
+
+console.log(completion.choices[0].message);
